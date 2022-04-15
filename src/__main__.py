98fd70a90e5bd0b41b1df9b74417650a2d87b0c6
Clint Plummer <clint@yuhonas.com>
2022-04-15 15:14:01 +1000

    initial version

--- src/__main__.py
+++ src/__main__.py
@@ -1,36 +1,56 @@
 import argparse
-from asyncio import subprocess
-from re import I
-from .colors import closest_colors_to
-from subprocess import run, PIPE
+from subprocess import run
+import logging
+from textwrap import dedent
+
+from .color_utils import closest_colors_to
+from . import settings
+
+logging.basicConfig(format='%(message)s', level=logging.INFO)
+logger = logging.getLogger()
 
 parser = argparse.ArgumentParser(
-  description='Gets and sets the limited number of colors apple allows us',
-  prog='osx-colors'
+  description='Sets the limited number of colors apple allows us too',
+  prog=settings.get_app_name(),
+  formatter_class=argparse.RawDescriptionHelpFormatter,
+  epilog=dedent(f'''
+    Example:
+    # To set osx to use Apple Green
+    $ {settings.get_app_name()} set 62ba46
+  ''')
 )
 
-parser.add_argument('color', help='The color to set in hex eg. 007aff leading # is optional')
-parser.add_argument('-r','--restart', action="store_true", help='Restart Finder and System Preferences after setting')
-parser.add_argument('-v', '--version', action='version', version='%(prog)s 1.0')
-
-# group = parser.add_mutually_exclusive_group()
-# group.add_argument('-s', '--set', help='The source color in hex eg. 007aff leading # is optional', choices=['pink','red','blue'])
-# group.add_argument('-c', '--set-custom', help='The source color in hex eg. 007aff leading # is optional', metavar='color')
+parser.add_argument('action', choices=['set'], help='action to perform')
+parser.add_argument('color', help='color to set in hex eg. 007aff leading # is optional')
+parser.add_argument('-s','--skip-restart',
+    action="store_true",
+    help='skip restarting Finder, Spotlight and System Preferences'
+)
+parser.add_argument('-v', '--version',
+    action='version',
+    version=f'%(prog)s {settings.get_version()}'
+)
 
 args = parser.parse_args()
 
-print(args)
-
 accent_color, highlight_color = closest_colors_to(args.color)
 
-cmd = ['defaults', 'write', '-g', 'AppleAccentColor', '-int', str(accent_color) ]
-print(cmd)
-print(run(cmd, stderr=PIPE, stdout=PIPE))
+logging.info("Setting the 'Accent Color' to the closest color to '%s'", args.color)
+logging.info("Setting the 'Highlight Color' to the closest color to '%s'", args.color)
 
-cmd = ['defaults', 'write', '-g', 'AppleHighlightColor', '-string', highlight_color ]
-print(cmd)
-run(cmd, stderr=PIPE, stdout=PIPE)
+logging.debug(
+  run(['defaults', 'write', '-g', 'AppleAccentColor', '-int', str(accent_color)], check=True)
+)
 
-if(args.restart):
-    print(run(['killall', 'Finder']))
-    print(run(['osascript', '-e', 'tell application "System Preferences" to quit']))
+logging.debug(
+  run(['defaults', 'write', '-g', 'AppleHighlightColor', '-string', highlight_color ], check=True)
+)
+
+if not args.skip_restart:
+    logging.info(dedent('''
+      Restarting Finder, Spotlight and System Preferences, others may need to be restarted manually
+    ''').strip())
+
+    run(['killall', 'Finder'], check=True)
+    run(['killall', 'Spotlight'], check=True)
+    run(['osascript', '-e', 'tell application "System Preferences" to quit'], check=True)
