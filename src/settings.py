b790c1f94bfb20fff98b1fede3523e5656035ee0
Clint Plummer <clint@yuhonas.com>
2022-04-17 19:32:18 +1000

    dont raise an exception if we can't get a version this was failing during test's as there's no package to get a version from, need to investigate a better way of handling this

--- src/settings.py
+++ src/settings.py
@@ -1,10 +1,15 @@
-from importlib.metadata import version
+import importlib.metadata as importlib_metadata
 
 APP_NAME = "osx-colors"  # TODO: Can we get this from the package meta data?
 
 
 def get_version():
-    return version(APP_NAME)
+    try:
+        version = importlib_metadata.version(APP_NAME)
+    except importlib_metadata.PackageNotFoundError:
+        version = "0.0.0"  # FIXME: No package to get a version from, we're probably in dev
+
+    return version
 
 
 def get_app_name():
